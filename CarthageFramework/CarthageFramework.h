e81164fd3e7e952e598574c881ee4fbc2e6d27fb
Kiruthika Selvavinayagam <Kiruthika.Selvavinayagam@makemytrip.com>
2017-03-26 15:44:09 +0530

    Initial Commit

---
+++ CarthageFramework/CarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  CarthageFramework.h
+//  CarthageFramework
+//
+//  Created by Kiruthika Selvavinayagam on 26/03/17.
+//  Copyright © 2017 Kiruthika Selvavinayagam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageFramework.
+FOUNDATION_EXPORT double CarthageFrameworkVersionNumber;
+
+//! Project version string for CarthageFramework.
+FOUNDATION_EXPORT const unsigned char CarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageFramework/PublicHeader.h>
+
+
